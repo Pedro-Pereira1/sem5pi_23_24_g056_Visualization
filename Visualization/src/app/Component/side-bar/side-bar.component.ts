2a001b2fbe65d1a3e07ba3cc2081023120dd8f1e
JoseGouveia <1211089@isep.ipp.pt>
2023-11-08 11:06:38 +0000

    Added initial main menu page

---
+++ Visualization/src/app/Component/side-bar/side-bar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+@Component({
+  selector: 'app-side-bar',
+  templateUrl: './side-bar.component.html',
+  styleUrls: ['./side-bar.component.css']
+})
+export class SideBarComponent {
+
+}
--- Visualization/src/app/app.module.ts
+++ Visualization/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainMenuComponent } from './Component/main-menu/main-menu.component';
+import { SideBarComponent } from './Component/side-bar/side-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainMenuComponent,
+    SideBarComponent
   ],
   imports: [
     BrowserModule,
