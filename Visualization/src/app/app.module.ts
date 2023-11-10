0e540b45a4ce2d9d422485287b4d3c716c469bc8
Pedro Pereira <1211131@isep.ipp.pt>
2023-11-10 16:16:31 +0000

    Added routing to all components

--- Visualization/src/app/app.module.ts
+++ Visualization/src/app/app.module.ts
@@ -4,15 +4,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BodyComponent } from './body/body.component';
 import { SidenavComponent } from './sidenav/sidenav.component';
-import { RobotsComponent } from './robots/robots.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
 import { SublevelMenuComponent } from './sidenav/sublevel-menu.component';
-import { FloorsComponent } from './floors/floors.component';
-import { PassagewaysComponent } from './passageways/passageways.component';
-import { RoomsComponent } from './rooms/rooms.component';
-import { ElevatorsComponent } from './elevators/elevators.component';
-import { RobotTypesComponent } from './robot-types/robot-types.component';
 import { View3dComponent } from './view3d/view3d.component';
 import { ShortestPathComponent } from './shortest-path/shortest-path.component';
 
@@ -21,14 +15,8 @@ import { ShortestPathComponent } from './shortest-path/shortest-path.component';
     AppComponent,
     BodyComponent,
     SidenavComponent,
-    RobotsComponent,
     HomeComponent,
     SublevelMenuComponent,
-    FloorsComponent,
-    PassagewaysComponent,
-    RoomsComponent,
-    ElevatorsComponent,
-    RobotTypesComponent,
     View3dComponent,
     ShortestPathComponent
   ],
