11da3676bc4b5116fd0c7260b515936fbe4a8f09
Pedro Pereira <1211131@isep.ipp.pt>
2023-11-13 15:11:24 +0000

    Updated listAllBuildingMaxMinFloor component, added table to show the intended buildings

--- Visualization/src/app/buildings/building-list-max-min/building-list-max-min.component.ts
+++ Visualization/src/app/buildings/building-list-max-min/building-list-max-min.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BuildingService } from 'src/app/services/building.service';
 
 @Component({
   selector: 'app-building-list-max-min',
   templateUrl: './building-list-max-min.component.html',
-  styleUrls: ['./building-list-max-min.component.css']
+  styleUrls: ['./building-list-max-min.component.css'],
+  providers: [BuildingService]
 })
-export class BuildingListMaxMinComponent {
+
+
+export class BuildingListMaxMinComponent implements OnInit {
+
+  buildings: any[] = [];
+
+  ngOnInit(): void {
+    
+  }
+
+  constructor(private buildingService: BuildingService) { }
+  max: number = 0;
+  min: number = 0;
+
+  listBuildingMaxMinFloors(){
+    this.buildingService.listBuildingMaxMinFloors(this.max, this.min).subscribe(
+      (data: any) => {
+        this.buildings = data;
+      },
+      (error: any) => {
+        console.error('Error:', error);
+        this.buildings = [];
+      }
+    );
+  }
 
 }
