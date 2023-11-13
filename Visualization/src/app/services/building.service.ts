5ef8cdae9a07a6c525117cce6f780159f8766e11
Pedro Pereira <1211131@isep.ipp.pt>
2023-11-13 15:09:55 +0000

    Added service building class

---
+++ Visualization/src/app/services/building.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BuildingService {
+
+  private buildingsUrl = "http://localhost:4000/api/buildings"
+
+  constructor(private http: HttpClient) { }
+
+  listBuildingMaxMinFloors(max: number, min: number) {
+    const url = this.buildingsUrl + "/" + "listBuildingsMaxMinFloors" + "/" + max + "/" + min;
+    return this.http.get(url);
+  }
+
+}
