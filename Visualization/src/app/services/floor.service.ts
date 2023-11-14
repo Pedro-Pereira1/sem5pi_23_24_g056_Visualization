c19484aa433cab660d83e911b19714c5604bad78
Pedro Pereira <1211131@isep.ipp.pt>
2023-11-14 18:01:22 +0000

    Add createFloor method to FloorService

--- Visualization/src/app/services/floor.service.ts
+++ Visualization/src/app/services/floor.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
+import { Floor } from '../domain/floor/Floor';
+import { FloorCreate } from '../domain/floor/FloorCreate';
 
 @Injectable({
   providedIn: 'root'
@@ -15,6 +17,11 @@ export class FloorService {
   listAllFloors(id: string) {
     const url = this.floorsUrl + "/" + "listAllFloors" + "/" + id;
     return this.http.get(url);
+  }
+
+  createFloor(floorToCreate: FloorCreate): Observable<Floor> {
+    const url = this.floorsUrl + "/" + "createFloor";
+    return this.http.post<Floor>(url, floorToCreate);
   }
 
 }
