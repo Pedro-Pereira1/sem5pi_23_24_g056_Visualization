d35461d749b3b196e1ee5bd4f2b1402660cccb12
Pedro Pereira <1211131@isep.ipp.pt>
2023-11-14 15:42:15 +0000

    Add listAll method to BuildingService

--- Visualization/src/app/services/building.service.ts
+++ Visualization/src/app/services/building.service.ts
@@ -17,4 +17,9 @@ export class BuildingService {
     return this.http.get(url);
   }
 
+  listAll() {
+    const url = this.buildingsUrl + "/" + "listAllBuildings";
+    return this.http.get(url);
+  }
+
 }
