6e4074847f6403be173dfa971e25f7e458f7caea
Alexandre Geração <1211151@isep.ipp.pt>
2023-11-15 15:43:00 +0000

    Added methods to list passageways in buildings to passageway-list-passageways-between-buildings.component class for US 1190

--- Visualization/src/app/passageways/passageway-list-passageways-between-buildings/passageway-list-passageways-between-buildings.component.ts
+++ Visualization/src/app/passageways/passageway-list-passageways-between-buildings/passageway-list-passageways-between-buildings.component.ts
@@ -1,10 +1,50 @@
 import { Component } from '@angular/core';
+import {BuildingService} from "../../services/building.service";
+import {PassagewayService} from "../../services/passageway.service";
+import {PassagewayList} from "../../domain/passageway/PassagewayList";
+import {Building} from "../../domain/building/Building";
 
 @Component({
   selector: 'app-passageway-list-passageways-between-buildings',
   templateUrl: './passageway-list-passageways-between-buildings.component.html',
-  styleUrls: ['./passageway-list-passageways-between-buildings.component.css']
+  styleUrls: ['./passageway-list-passageways-between-buildings.component.css'],
+  providers: [PassagewayService, BuildingService]
 })
 export class PassagewayListPassagewaysBetweenBuildingsComponent {
+  constructor(private passagewayService: PassagewayService, private buildingService:BuildingService) { }
+  passageways: PassagewayList[] = [];
+  buildings: Building[] = [];
+  building1: string = "";
+  building2: string = "";
 
+  ngOnInit(): void {
+    this.buildingService.listAll().subscribe(
+      (data: any) => {
+        this.buildings = data;
+      },
+      (error: any) => {
+        console.error('Error:', error);
+        this.buildings = [];
+      }
+    );
+  }
+
+  listPassagewaysBuildings(){
+    if(this.building1 == "" || this.building2 == ""){
+      alert("Please select both buildings");
+      return;
+    }else if(this.building1 == this.building2){
+      alert("Please select two different buildings");
+      return;
+    }
+    this.passagewayService.listPassagewaysBuildings(this.building1,this.building2).subscribe(
+      (data: any) => {
+        this.passageways = data;
+      },
+      (error: any) => {
+        console.error('Error:', error);
+        this.passageways = [];
+      }
+    );
+  }
 }
