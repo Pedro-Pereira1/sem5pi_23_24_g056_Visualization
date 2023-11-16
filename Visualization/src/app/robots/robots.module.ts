ee5fcfe8a635ce8b6fded7d1bb78c57a974822ea
Pedro Pereira <1211131@isep.ipp.pt>
2023-11-16 22:01:11 +0000

    Add ReactiveFormsModule to FormsModule in robots.module.ts relative to US1010

--- Visualization/src/app/robots/robots.module.ts
+++ Visualization/src/app/robots/robots.module.ts
@@ -5,7 +5,7 @@ import { RobotsRoutingModule } from './robots-routing.module';
 import { RobotInhibitComponent } from './robot-inhibit/robot-inhibit.component';
 import { RobotListAllComponent } from './robot-list-all/robot-list-all.component';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RobotTypeService } from '../services/robot-type.service';
 import { RobotService } from '../services/robot.service';
 import { RobotsComponent } from './robots.component';
@@ -21,7 +21,8 @@ import { RobotsComponent } from './robots.component';
     CommonModule,
     RobotsRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [RobotTypeService,RobotService]
 
