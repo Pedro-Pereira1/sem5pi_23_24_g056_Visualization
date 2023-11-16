1379ae43f7c303b92dad9adf59321a7f2eb9e054
Alexandre Geração <1211151@isep.ipp.pt>
2023-11-16 10:46:33 +0000

    Added modules to rooms.module class for US 1210

--- Visualization/src/app/rooms/rooms.module.ts
+++ Visualization/src/app/rooms/rooms.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { RoomsRoutingModule } from './rooms-routing.module';
-
+import {RoomsComponent} from "./rooms.component";
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {BuildingService} from "../services/building.service";
+import {RoomService} from "../services/room.service";
 
 @NgModule({
-  declarations: [],
+  declarations: [RoomsComponent],
   imports: [
     CommonModule,
-    RoomsRoutingModule
-  ]
+    RoomsRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [RoomService, BuildingService]
+
 })
 export class RoomsModule { }
