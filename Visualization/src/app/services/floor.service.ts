b0d1a3500d8f21b8b03927a0ee4b3defcdba14c4
Pedro Pereira <1211131@isep.ipp.pt>
2023-11-16 21:33:21 +0000

    Add error handling to FloorService HTTP requests

--- Visualization/src/app/services/floor.service.ts
+++ Visualization/src/app/services/floor.service.ts
@@ -19,19 +19,56 @@ export class FloorService {
 
   listAllFloors(id: string) {
     const url = this.floorsUrl + "/" + "listAllFloors" + "/" + id;
-    return this.http.get(url);
+    return this.http.get(url).pipe(
+      catchError((error: HttpErrorResponse) => {
+        let errorMessage = '';
+        if (error.error instanceof ErrorEvent) {
+          errorMessage = `An error occurred: ${error.error.message}`;
+          window.alert(errorMessage);
+        } else {
+          errorMessage = `An error occurred: ${error.error}`;
+          window.alert(errorMessage);
+        }
+        console.error(errorMessage);
+        return throwError(errorMessage);
+      })
+    );
   }
 
   createFloor(floorToCreate: FloorCreate): Observable<Floor> {
     const url = this.floorsUrl + "/" + "createFloor";
-    return this.http.post<Floor>(url, floorToCreate);
+    return this.http.post<Floor>(url, floorToCreate).pipe(
+      catchError((error: HttpErrorResponse) => {
+        let errorMessage = '';
+        if (error.error instanceof ErrorEvent) {
+          errorMessage = `An error occurred: ${error.error.message}`;
+          window.alert(errorMessage);
+        } else {
+          errorMessage = `An error occurred: ${error.error}`;
+          window.alert(errorMessage);
+        }
+        console.error(errorMessage);
+        return throwError(errorMessage);
+      })
+    );
   }
 
   editFloor(floorToEdit: FloorEdit): Observable<Floor> {
     const url = this.floorsUrl + "/" + "editFloor";
     return this.http.put<Floor>(url, floorToEdit).pipe(
-      catchError(this.handleError)
-    )
+      catchError((error: HttpErrorResponse) => {
+        let errorMessage = '';
+        if (error.error instanceof ErrorEvent) {
+          errorMessage = `An error occurred: ${error.error.message}`;
+          window.alert(errorMessage);
+        } else {
+          errorMessage = `An error occurred: ${error.error}`;
+          window.alert(errorMessage);
+        }
+        console.error(errorMessage);
+        return throwError(errorMessage);
+      })
+    );
   }
 
   loadFloorMap(loadFloorMap: LoadFloorMap): Observable<Floor> {
