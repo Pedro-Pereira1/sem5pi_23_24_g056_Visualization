abd86bb64bd7904dd9243ada5276235f166bcacc
Tiago <1211128@isep.ipp.pt>
2023-11-19 15:12:32 +0000

    Added elevator representations for edit and listing

---
+++ Visualization/src/app/domain/elevator/ElevatorEdit.ts
@@ -0,0 +1,10 @@
+export interface ElevatorEdit {
+    elevatorIdentificationNumber: number
+    elevatorBrand: string
+    elevatorDescription: string
+    elevatorModel: string
+    elevatorSerialNumber: string
+    buildingCode: string
+    floorsIdToAdd: number[]
+    floorsIdToRemove: number[]
+}
