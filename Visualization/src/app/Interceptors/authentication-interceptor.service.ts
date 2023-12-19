6ea8308f62919aae1345a0a24cf7aeb99c1529f6
JoseGouveia <1211089@isep.ipp.pt>
2023-12-19 11:04:27 +0000

    Added an interceptor to insert into the http request header a token

---
+++ Visualization/src/app/Interceptors/authentication-interceptor.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { AuthServiceService } from '../services/auth-service.service';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationInterceptorService implements HttpInterceptor {
+
+  constructor(private authService: AuthServiceService) { }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const token = this.authService.getToken();
+
+    if (token) {
+      const cloned = req.clone({headers: req.headers.set('Authorization', 'Bearer ' + token)});
+      return next.handle(cloned);
+    } else {
+      return next.handle(req);
+    }
+  }
+
+
+}
