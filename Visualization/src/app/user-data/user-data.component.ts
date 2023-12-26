12b1726ce03467fc39bc154ea5aed34619a63829
Pedro Pereira <1211131@isep.ipp.pt>
2023-12-26 20:38:36 +0000

    SC-27 Add OnInit interface and implement user profile functionality and function to dowload a json file

--- Visualization/src/app/user-data/user-data.component.ts
+++ Visualization/src/app/user-data/user-data.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { CreateBackofficeUserDto } from '../domain/user/CreateBackofficeUserDto';
 import { UserDto } from '../domain/user/UserDto';
@@ -9,14 +9,16 @@ import { AuthServiceService } from '../services/auth-service.service';
   templateUrl: './user-data.component.html',
   styleUrl: './user-data.component.css'
 })
-export class UserDataComponent {
+export class UserDataComponent implements OnInit {
   constructor(
     private authService: AuthServiceService
   ) { }
 
-  roleTypes: string[] = ["Admin", "Campus Manager", "Fleet Manager", "Task Manager"];
 
-  createForm = new FormGroup({
+  roleTypes: string[] = ["Admin", "Campus Manager", "Fleet Manager", "Task Manager"];
+  passwordVisibility = false;
+
+  userForm = new FormGroup({
     name: new FormControl(""),
     email: new FormControl(""),
     phoneNumber: new FormControl(""),
@@ -24,22 +26,61 @@ export class UserDataComponent {
     role: new FormControl("")
   });
 
-  onSubmit() {
-    console.log(this.createForm.value);
-    
-    const user: CreateBackofficeUserDto = {
-      name: this.createForm.value.name!,
-      email: this.createForm.value.email!,
-      phoneNumber: Number(this.createForm.value.phoneNumber!),
-      password: this.createForm.value.password!,
-      role: this.createForm.value.role!
+  ngOnInit(): void {
+    this.userForm.disable();
+    const userEmail = this.authService.getEmailByToken(this.authService.getToken())
+    this.authService.getUserInfo(userEmail).subscribe(userInfo => {
+      this.userForm.controls['name'].setValue(userInfo.name)
+      this.userForm.controls['email'].setValue(userEmail)
+      this.userForm.controls['phoneNumber'].setValue(String(userInfo.phoneNumber))
+    });
+  }
+
+
+  editProfile(){
+    this.userForm.enable();
+    this.passwordVisibility = true;
+
+  }
+
+  deleteProfile(){
+  }
+
+  dowloadData(){
+    let user: UserDto = {
+      name: "",
+      email: "",
+      phoneNumber: 0,
+      taxPayerNumber: 0,
+      role: "",
     }
 
-    this.authService.createBackofficeUser(user).subscribe(
-      (user: UserDto) => {
-        window.alert('User created successfully');
-        this.createForm.reset();
-      }
-    );
+    const userEmail = this.authService.getEmailByToken(this.authService.getToken())
+    this.authService.getUserInfo(userEmail).subscribe(userInfo => {
+      user.name = userInfo.name;
+      user.email = userEmail;
+      user.phoneNumber = userInfo.phoneNumber;
+      user.taxPayerNumber = userInfo.taxPayerNumber;
+      user.role = userInfo.role;
+      const fileContent = JSON.stringify(user);
+
+      const blob = new Blob([fileContent], { type: 'text/plain' });
+
+      const link = document.createElement('a');
+      link.href = URL.createObjectURL(blob);
+      
+      const dateTime = new Date().toLocaleString();
+      const date = dateTime.split(',')[0].replaceAll('/', '');
+      const time = dateTime.split(',')[1].replaceAll(':', '');
+      
+      link.download = 'MyData_' + date +'T'+ time + '.json';
+      
+      document.body.appendChild(link);
+      
+      link.click();
+      
+      document.body.removeChild(link);
+    });
+
   }
 }
