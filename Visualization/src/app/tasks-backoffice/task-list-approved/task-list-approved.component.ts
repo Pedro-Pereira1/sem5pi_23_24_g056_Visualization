609bf1a8707fed0669cf658be4f8ef11cb6882f8
Tiago <1211128@isep.ipp.pt>
2023-12-27 21:57:22 +0000

    Add task-list-approved component

---
+++ Visualization/src/app/tasks-backoffice/task-list-approved/task-list-approved.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-list-approved',
+  templateUrl: './task-list-approved.component.html',
+  styleUrl: './task-list-approved.component.css'
+})
+export class TaskListApprovedComponent {
+
+}
