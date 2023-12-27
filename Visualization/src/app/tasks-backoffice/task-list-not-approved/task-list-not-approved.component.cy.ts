0cd1f7159640543ba3ff3ce55e2a53526ee72b9d
Tiago <1211128@isep.ipp.pt>
2023-12-27 21:57:40 +0000

    Add task-list-not-approved component

---
+++ Visualization/src/app/tasks-backoffice/task-list-not-approved/task-list-not-approved.component.cy.ts
@@ -0,0 +1,7 @@
+import { TaskListNotApprovedComponent } from './task-list-not-approved.component'
+
+describe('TaskListNotApprovedComponent', () => {
+  it('should mount', () => {
+    cy.mount(TaskListNotApprovedComponent)
+  })
+})
---
+++ Visualization/src/app/tasks-backoffice/task-list-not-approved/task-list-not-approved.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-list-not-approved',
+  templateUrl: './task-list-not-approved.component.html',
+  styleUrl: './task-list-not-approved.component.css'
+})
+export class TaskListNotApprovedComponent {
+
+}
