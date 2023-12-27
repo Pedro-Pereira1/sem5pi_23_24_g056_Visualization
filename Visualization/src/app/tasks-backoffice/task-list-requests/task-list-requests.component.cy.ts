3880ebe11e96a6b606a2b0f3f919f21c0475ec4b
Tiago <1211128@isep.ipp.pt>
2023-12-27 21:58:11 +0000

    Add task-list-requests component

---
+++ Visualization/src/app/tasks-backoffice/task-list-requests/task-list-requests.component.cy.ts
@@ -0,0 +1,7 @@
+import { TaskListRequestsComponent } from './task-list-requests.component'
+
+describe('TaskListRequestsComponent', () => {
+  it('should mount', () => {
+    cy.mount(TaskListRequestsComponent)
+  })
+})
