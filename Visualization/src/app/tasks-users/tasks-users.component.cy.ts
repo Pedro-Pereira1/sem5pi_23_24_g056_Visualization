83ccfb641ff9f81701f94f460dc166e7420f9420
Tiago <1211128@isep.ipp.pt>
2023-12-28 20:57:48 +0000

    Add tasks-users component files

---
+++ Visualization/src/app/tasks-users/tasks-users.component.cy.ts
@@ -0,0 +1,7 @@
+import { TasksUsersComponent } from './tasks-users.component'
+
+describe('TasksUsersComponent', () => {
+  it('should mount', () => {
+    cy.mount(TasksUsersComponent)
+  })
+})
---
+++ Visualization/src/app/tasks-users/tasks-users.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tasks-users',
+  templateUrl: './tasks-users.component.html',
+  styleUrl: './tasks-users.component.css'
+})
+export class TasksUsersComponent {
+
+}
